db4aa8e17011c585106ed60bddcb537c05e5c77b
ad-hz1-wacai\qinan <qinan@wacai.com>
2017-03-14 13:09:46 +0800

    softmax完成

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -29,7 +29,33 @@ def softmax_loss_naive(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  num_train=X.shape[0]
+  num_dim=X.shape[1]
+  num_classes=W.shape[1]
+  score=X.dot(W)
+  for i in xrange(num_train):
+    correct_class=y[i]
+    sum_exp_score=0.0
+    max_score=-np.Inf
+    for j in xrange(num_classes):
+      max_score=max(max_score,score[i,j])
+    for j in xrange(num_classes):
+      score[i,j]=np.exp(score[i,j]-max_score)
+      sum_exp_score+=score[i,j]
+    for j in xrange(num_classes):
+      if j==correct_class:
+        dW[:,j]+=(-sum_exp_score/score[i,correct_class])*((sum_exp_score-score[i,j])*score[i,j]/(sum_exp_score**2)*X[i,:])
+      else:
+        dW[:,j]+=(-sum_exp_score/score[i,correct_class])*(-score[i,correct_class]*score[i,j]/(sum_exp_score**2)*X[i,:])
+    loss+=-np.log(score[i,correct_class]/sum_exp_score)
+  loss/=num_train
+  W_sum=0.0
+  for i in xrange(num_dim):
+    for j in xrange(num_classes):
+      W_sum+=W[i,j]**2
+  loss+=0.5*reg*W_sum
+  dW/=num_train
+  dW+=reg*W
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
@@ -53,10 +79,31 @@ def softmax_loss_vectorized(W, X, y, reg):
   # here, it is easy to run into numeric instability. Don't forget the        #
   # regularization!                                                           #
   #############################################################################
-  pass
+  num_train=X.shape[0]
+  score=X.dot(W)
+  score-=np.max(score,1,keepdims=True)
+  score=np.exp(score)
+  loss_i=score[xrange(num_train),y]/np.sum(score,1)
+  loss=np.sum(-np.log(loss_i))/num_train+0.5*reg*np.sum(np.square(W))
+  partion_deriv=-(-1.0/loss_i).reshape(num_train,1)*score*score[xrange(num_train),y].reshape(num_train,1)/(np.sum(score,1,keepdims=True)**2)
+  partion_deriv[xrange(num_train),y]*=-(np.sum(score,1)-score[xrange(num_train),y])/score[xrange(num_train),y]
+  dW=X.T.dot(partion_deriv)
+  dW/=num_train
+  dW+=reg*W
   #############################################################################
   #                          END OF YOUR CODE                                 #
   #############################################################################
 
   return loss, dW
 
+if __name__ == '__main__':
+  score=np.array([[1,2,3],[2,4,3],[3,4,5],[4,5,6]])
+  y=np.array([1,1,1,2])
+  # score-=np.max(score,1,keepdims=True)
+  # score=np.exp(score)
+  # print score[xrange(score.shape[0]),y]/np.sum(score,1)
+  # print score
+  # print score[xrange(score.shape[0]),y].reshape(score.shape[0],1)
+  # print score*score[xrange(score.shape[0]),y].reshape(score.shape[0],1)
+  # print score*y.reshape(score.shape[0],1)
+  print 1.0/y
